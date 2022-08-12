234d16aa049363bcf5bec6632983b13921589d7b
ali.ebadian <ali.ebadian@infinityworks.com>
2022-08-12 17:16:23 +0100

    Adding a little comments

--- 1-HelloWorld/hello_test.go
+++ 1-HelloWorld/hello_test.go
@@ -12,3 +12,11 @@ func TestHello(t *testing.T) {
 }
 
 //To run the test you need to run "go mod init hello" first.
+//Writing a test is just like writing a function, with a few rules
+//It needs to be in a file with a name like xxx_test.go
+//The test function must start with the word Test
+//The test function takes one argument only t *testing.T
+//In order to use the *testing.T type, you need to import "testing", like we did with fmt in the other file.
+//*testing.T is your "hook" into the testing framework, so you can do things like t.Fail()
+
+// varName := value <- Variable declaration in Go
