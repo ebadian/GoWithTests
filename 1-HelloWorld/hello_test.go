8115446a4027df228a9061f8906fa1d7ca06a2f2
ali.ebadian <ali.ebadian@infinityworks.com>
2022-08-15 14:42:40 +0100

    Added a test and the code to have world as default value for name.

--- 1-HelloWorld/hello_test.go
+++ 1-HelloWorld/hello_test.go
@@ -1,22 +1,27 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people ", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
 
-	if got != want {
-		t.Errorf("Got %q but wanted %q", got, want)
-	}
+	t.Run("say 'Hello world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
-//To run the test you need to run "go mod init hello" first.
-//Writing a test is just like writing a function, with a few rules
-//It needs to be in a file with a name like xxx_test.go
-//The test function must start with the word Test
-//The test function takes one argument only t *testing.T
-//In order to use the *testing.T type, you need to import "testing", like we did with fmt in the other file.
-//*testing.T is your "hook" into the testing framework, so you can do things like t.Fail()
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 
-// varName := value <- Variable declaration in Go
+}
